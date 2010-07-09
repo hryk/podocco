f4bb134d5a3353b1c415b10c5ec47b2698905987
hryk <storage.hn@gmail.com>
2010-07-09 14:19:53 +0900

    improved parsing code.

--- lib/Pod/Docco.pm
+++ lib/Pod/Docco.pm
@@ -15,30 +15,6 @@ use Pod::Docco::Layout;
 carp "WARNING: Pygments not found. Using webservice." 
   unless which('pygmentize');
 
-=head1 NAME
-
-Pod::Docco
-
-=head1 SYNOPSYS
-
-    podocco lib/*.py
-
-=head1 DESCRIPTION
-
-Podocco is a Perl port of L<Docco|http://jashkenas.github.com/docco/>: the original
-quick-and-dirty documentation generator.
-
-It produces HTML that displays your comments
-alongside your code. Comments are passed through L<Pod>, and code is passed through
-Pyhmemnts syntax highlighting. This page is the result of Pod::Docco against its own
-source file.
-
-=cut
-
-=head1 Methods
-
-=cut
-
 has 'filename' => (
   is       => 'rw',
   isa      => 'Str',
@@ -74,13 +50,36 @@ has 'data' => (
   required => 0
 );
 
+=head1 NAME
+
+Pod::Docco
+
+=head1 SYNOPSYS
+
+    podocco lib/*.py
+
+=head1 DESCRIPTION
+
+Podocco is a Perl port of L<Docco|http://jashkenas.github.com/docco/>: the original
+quick-and-dirty documentation generator.
+
+It produces HTML that displays your comments
+alongside your code. Comments are passed through L<Pod>, and code is passed through
+Pyhmemnts syntax highlighting. This page is the result of Pod::Docco against its own
+source file.
+
+=head1 Methods
+
+=cut
+
 sub BUILD {
   my $self = shift;
   my $data = PPI::Document->new($self->filename);
   $data->prune('PPI::Token::Comment');
   $self->data($data);
-  $self->sections($self->highlight($self->split($self->parse)));
+  $self->sections($self->highlight($self->split_tuples($self->parse)));
 }
+
 
 =head2 to_html
 
@@ -132,7 +131,7 @@ sub highlight {
   $code_html = [
   map { (my $x = $_ ) =~ s/\n*?<\/pre><\/div>\n//; $x }
   map { (my $x = $_ ) =~ s/\n*?<div\sclass=\"highlight\"><pre>//; $x}
-  split(/\n<span\sclass=\"c.\">#------BOUNDARY------<\/span>\n/, $code_html)
+  split(/\n*<span\sclass=\"c.\">#------BOUNDARY------<\/span>\n*/, $code_html)
   ];
 
   # zip code and docs.
@@ -166,7 +165,7 @@ sub highlight_pygmentize {
 
 =cut
 
-sub split {
+sub split_tuples {
   my $self     = shift;
   my $sections = shift;
   my ( $docs_blocks , $code_blocks ) = ([], []);
@@ -199,34 +198,62 @@ is a shebang line.
 
 =cut
 
+# pod block start 1
+# pod block end   10
+# code blocks start 11
+# code blocks end   11+n
+#
+# pod2 start 20
+# pod2 end
+# code start
+
 sub parse {
   my $self = shift;
   my $sections = [];
-  my $start_lnum = 0;
+  my ($prev_pod, $prev_code_start);
 
   for my $pod (@{$self->data->find(sub{$_[1]->isa('PPI::Token::Pod')})}) {
-    my ($docs, $code) = ([],[]);
-    my $statements = $self->data->find(sub{
-        ( $_[1]->line_number > $start_lnum )        and
-        ( $_[1]->line_number < $pod->line_number )  and
-        ( $_[1]->class eq 'PPI::Statement' )
-      });
-
-    if ($statements) {
-      for my $state (@$statements) {
-        my $text = $state->content."\n\n";
-        push @$code, split("\n", $text);
-      }
+    # skip unless prev_pod.
+    if ($prev_pod) {
+      my ($docs, $code) = ([], []);
+      # print "prev_code : $prev_code_start\n";
+      # print "prev_pod  : ".( $pod->line_number -1 ) ."\n";
+      $code = $self->get_code_blocks($prev_code_start, $pod->line_number - 1);
+      push @$docs, split("\n", $prev_pod->content);
+      push @$sections, [$docs,$code];
     }
-    else {
-      $code = [];
-    }
-    push @$docs, split("\n", $pod->content);
-    push @$sections, [$docs,$code];
-    $start_lnum = $pod->line_number;
+    $prev_pod = $pod;
+    $prev_code_start = $pod->snext_sibling->line_number;
   }
 
+  push @$sections, [
+    [split("\n", $prev_pod->content)],
+    $self->get_code_blocks($prev_code_start, $self->data->last_element->line_number)
+  ];
+
   return $sections;
+}
+
+sub get_code_blocks {
+  my $self = shift;
+  my ($start, $end) = @_;
+  my $code = [];
+  my $statements = $self->data->find(sub{
+      ($_[1]->parent == $_[0]) and
+      (
+        $_[1]->line_number >= $start and
+        $_[1]->line_number <  $end
+      )
+    });
+
+  if ($statements) {
+    for my $state (@$statements) {
+      my $text = $state->content."\n\n";
+      push @$code, split("\n", $text);
+    }
+  }
+
+  $code;
 }
 
 no Any::Moose;
